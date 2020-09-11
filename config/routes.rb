65484756a14f835812e625fe723fbd7c2a381099
natsume4948 <kazu.100817@gmail.com>
2020-09-11 13:28:33 +0000

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/new'
+  get 'users/create'
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
 end
